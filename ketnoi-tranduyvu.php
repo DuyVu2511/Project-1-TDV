b5b11be4fe79875bb3105c491bcb1b8ad955be7d
DuyVu2511 <duyvutran2004@gmail.com>
2023-12-11 14:13:27 +0700

    project 1

--- ketnoi-tranduyvu.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn_tdv = new mysqli("localhost","root","","k22cntt3_tranduyvu_day11");
-    if($conn_tdv->errno){
-        echo "Lỗi kết nối, ". $conn_tdv->error;
-    }else{
-        echo "<h1> Xin chào, Tran Duy Vu - 2210900138 </h1>";
-    }
-?>
